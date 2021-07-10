6f376232a3baaeced9a38b787ddba28c24e2d95c
SarvarKhalimov <khalimovsarvar@gmail.com>
2021-07-10 11:48:17 +0500

    SetState of square value to 'x' onClick

--- my-app/src/index.js
+++ my-app/src/index.js
@@ -3,10 +3,19 @@ import ReactDOM from 'react-dom';
 import './index.css';
 
 class Square extends React.Component {
+  constructor(props){
+    super(props);
+    this.state = {
+      value: null
+    }
+  }
   render() {
     return (
-      <button className="square">
-        {/* TODO */}
+      <button 
+        className="square" 
+        onClick={() => this.setState({value: 'X'})}
+      >
+        {this.state.value}
       </button>
     );
   }
@@ -14,7 +23,7 @@ class Square extends React.Component {
 
 class Board extends React.Component {
   renderSquare(i) {
-    return <Square />;
+    return <Square value={i} />;
   }
 
   render() {
